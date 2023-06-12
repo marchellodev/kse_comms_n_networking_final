9cb750d44b7b4278608aa30c8b0d4d05e02ed687
marchellodev <marchellodev@gmail.com>
2023-06-12 12:11:44 +0300

    impl ping-pong

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 use std::{
-    collections::HashMap,
     env,
-    io::{prelude::*, BufReader, BufWriter, Cursor},
+    io::{self, prelude::*, BufReader, BufWriter, Cursor},
     net::{TcpListener, TcpStream},
-    sync::{Arc, Mutex},
     thread,
 };
 
@@ -22,7 +20,58 @@ fn main() {
 }
 
 fn run_client() {
-    println!("RUNNIG CLIENT")
+    let stream = TcpStream::connect("127.0.0.1:7878").unwrap();
+
+    let desc = r#"
+        Available commands:
+        > ping
+        > <number>
+    "#;
+
+    println!("Connected to the server!");
+    println!("{}", desc);
+
+    let mut reader = BufReader::new(stream.try_clone().unwrap());
+    let mut writer = stream.try_clone().unwrap();
+
+    let stdin = io::stdin();
+    let mut input = String::new();
+    loop {
+        input.clear();
+        stdin.read_line(&mut input).unwrap();
+
+        let input = input.trim();
+
+        match input {
+            "ping" => {
+                writer.write_u32::<BigEndian>(PING).unwrap();
+            }
+            _ => {
+                println!("sending a numebr");
+                let num = input.parse::<u32>().unwrap();
+                writer.write_u32::<BigEndian>(num).unwrap();
+            }
+        }
+
+        println!("Sending user input: {}", input);
+
+        let mut buffer = [0; 4];
+
+        if reader.read_exact(&mut buffer).is_err() {
+            println!("Connection closed");
+            break;
+        }
+        let mut rdr = Cursor::new(buffer);
+        let decoded = rdr.read_u32::<BigEndian>().unwrap();
+        match decoded {
+            PONG => {
+                println!("> Received pong from the server");
+            }
+            _ => {
+                println!("Unknown response")
+            }
+        }
+    }
 }
 fn run_server() {
     println!("RUNNING SERVER");
